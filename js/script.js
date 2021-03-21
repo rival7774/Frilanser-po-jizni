55ef5445e8febf7783d2d1592cebd5711cbc11db
Samir <sakitov@yandex.ru>
2021-03-21 16:32:00 +0400

    Добавил папки с уроками(теорией)

--- js/script.js
+++ js/script.js
@@ -2,6 +2,56 @@
 
 let userAge = `45` * `-8`;
 let userInfo = `Фрилансер ${userAge}`;
-console.log(userAge);
-console.log(userAge);
-console.log(userAge);
+console.log(userAge)
+
+let a = 1 + 2;
+let b = 2;
+
+let rezult = 8 - (a = b + 3);
+console.log(a);
+
+
+
+
+// console.log('35' + -'22');
+// // 13
+
+// console.log('35' * '22');
+// // 770
+
+// console.log('35' > '22');
+// // true
+
+// let userCounter = 0;
+// let newUser = userCounter++;
+// console.log(newUser);
+// // 0
+
+// console.log(!false && 11 || 18 && !'');
+// // 11
+
+// let namme = 0;
+// console.log(namme ?? 'без');
+
+
+
+// if (1 === "1") {
+//    console.log('Истина!');
+// } else {
+//    console.log('Ложь!');
+// }
+
+// if (5 == "5") {
+//    console.log('Истина!');
+// } else {
+//    console.log('Ложь!');
+// }
+
+// let message = (92 > '11' && 58 < 100) ? 'Истина!' : 'Ложь!';
+// console.log(message);
+
+// if (0) {
+//    console.log('Ложь!');
+// } else if (" ") {
+//    console.log('Истина!');
+// }
